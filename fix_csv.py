a3d62eaae097a30709b74ed3f8ad3282c61c4204
sariVagshal <sari6910@gmail.com>
2018-12-11 15:26:24 +0200

    changed to a function

--- fix_csv.py
+++ fix_csv.py
@@ -1,20 +1,21 @@
 import csv
 
-flag = True
-with open('data/sample.csv', 'rt',encoding="utf-8", errors='ignore') as ifile, open('data/fixed.csv', 'wt',newline='',encoding="utf-8") as ofile:
-    reader = csv.reader(ifile, delimiter=',')
-    writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+def fix_csv_file(file_name):
+    flag = True
+    with open(file_name, 'rt',encoding="utf-8", errors='ignore') as ifile, open('data/fixed.csv', 'wt',newline='',encoding="utf-8") as ofile:
+        reader = csv.reader(ifile, delimiter=',')
+        writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
-    for row in reader:
-        if flag:
-            flag=False
-            continue
+        for row in reader:
+            if flag:
+                flag=False
+                continue
 
-        able = True
-        if len(row) != 14:
-            able = False
-        for coll in row:
-            if not coll:
+            able = True
+            if len(row) != 14:
                 able = False
-        if able:
-            writer.writerow(row)
+            for coll in row:
+                if not coll:
+                    able = False
+            if able:
+                writer.writerow(row)
