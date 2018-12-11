bffc627822cb30df93eb17907d059a30c69b761d
sariVagshal <sari6910@gmail.com>
2018-12-11 15:24:09 +0200

    fixed the blank lines

--- fix_csv.py
+++ fix_csv.py
@@ -1,7 +1,7 @@
 import csv
 
 flag = True
-with open('data/sample.csv', 'rt',encoding="utf-8", errors='ignore') as ifile, open('data/fixed.csv', 'wt',encoding="utf-8") as ofile:
+with open('data/sample.csv', 'rt',encoding="utf-8", errors='ignore') as ifile, open('data/fixed.csv', 'wt',newline='',encoding="utf-8") as ofile:
     reader = csv.reader(ifile, delimiter=',')
     writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
@@ -9,8 +9,6 @@ with open('data/sample.csv', 'rt',encoding="utf-8", errors='ignore') as ifile, o
         if flag:
             flag=False
             continue
-
-        print(row)
 
         able = True
         if len(row) != 14:
