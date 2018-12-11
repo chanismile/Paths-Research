39017eb3e389ad902124ee14f25d5cda9ebb2482
chanismile <37192892+chanismile@users.noreply.github.com>
2018-12-11 15:01:39 +0200

    stamm

--- fix_csv.py
+++ fix_csv.py
@@ -1,7 +1,16 @@
 import csv
+import fileinput
+
+flag = True
+counter = 0;
 with open('data/oddetect.csv', 'rb') as ifile, open('data/fixed.csv', 'wb') as ofile:
-    csv_reader = csv.reader(ifile, delimiter=',')
-    reader = csv.reader(ifile)
-    writer = csv.writer(ofile, delimiter='', quotechar='"', quoting=csv.QUOTE_ALL)
+    reader = csv.reader(ifile, delimiter=',')
+    writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    for line in ifile:
+        process(line)
     for row in reader:
-        writer.writerow(row)
+        if (flag):
+            flag = False
+            continue
+        counter +=1
+        print(row)
