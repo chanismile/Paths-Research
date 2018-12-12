69ef1e38de903253c14d635824146fa45405d998
chanismile <37192892+chanismile@users.noreply.github.com>
2018-12-12 21:29:41 +0200

    add the filters to controller

--- paths_project/tests.py
+++ paths_project/tests.py
@@ -4,11 +4,11 @@ import controller
 class PathProjectTestCase(unittest.TestCase):
     def test_get_file(self):
         control = controller.Controller('../data/paths.pkl.xz')
-        file = control.get_file()
+
 
     def test_drow(self):
-        control = controller.Controller('../data/paths.pkl.xz')
-        control.drow([([30,100,150,200,250],[30,40,60,90,150]),([350,300],[30,50]),([400,300],[30,100]),([250,150],[30,50]),([100,250],[30,50])],False)
+        control = controller.Controller('../data/paths.pkl.xz','../data/paths0.png')
+        control.draw_lines([([30,100,150,200,250],[30,40,60,90,150]),([350,300],[30,50]),([400,300],[30,100]),([250,150],[30,50]),([100,250],[30,50])],False)
 
     def test_drow_by_filter(self):
         pass
